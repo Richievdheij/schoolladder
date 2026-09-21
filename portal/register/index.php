4281b2142f66405d42934b14af17ccdb19c5d542
Martijn Swarte <martijn.swarte@gmail.com>
2026-09-21 13:17:18 +0200

    [WIP] Login & Register gemaakt tot database

--- portal/register/index.php
+++ portal/register/index.php
@@ -1,10 +1,17 @@
 <?php
-$pageTitle = 'Login | Schoolladder';
-$errors = [];
+$pageTitle = 'Register | Schoolladder';
+
+$errors = null;
 
 if (isset($_POST['submit'])) {
+    $name = htmlentities($_POST['name']);
     $email = htmlentities($_POST['email']);
     $password = htmlentities($_POST['password']);
+    $password_confirm = htmlentities($_POST['password-confirm']);
+
+    if (!isset($name) || $name === '') {
+        $errors[] = "Naam is verplicht";
+    }
 
     if (!isset($email) || $email === '') {
         $errors[] = "E-mailadres is verplicht";
@@ -12,6 +19,14 @@ if (isset($_POST['submit'])) {
 
     if (!isset($password) || $password === '') {
         $errors[] = "Wachtwoord is verplicht";
+    }
+
+    if (!isset($password_confirm) || $password_confirm === '') {
+        $errors[] = "Wachtwoord herhaling is verplicht";
+    }
+
+    if ($password !== $password_confirm) {
+        $errors[] = "Wachtwoorden moeten hetzelfde zijn";
     }
 }
 
@@ -23,7 +38,7 @@ if (isset($_POST['submit'])) {
 <body>
     <main style="display: flex; flex-direction: column; gap: 1em;">
         <div class="container">
-            <h1>Login</h1>
+            <h1>Register</h1>
             <p>De plek waar je alles kunt zien wat je nodig hebt voor school!</p>
         </div>
         <form action="" method="post" style="display: flex; flex-direction: column; gap: 1em;">
@@ -35,19 +50,30 @@ if (isset($_POST['submit'])) {
                 </div>
             <?php endif; ?>
             <div class="container section">
+                <div class="<?= (isset($name) && $name === '') ? 'error-input' : ''; ?>">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" placeholder="John Doe" value="<?= (isset($name) && $name !== '') ? $name : '' ?>">
+                </div>
                 <div class="<?= (isset($email) && $email === '') ? 'error-input' : ''; ?>">
                     <label for="email">Email</label>
                     <input type="email" name="email" placeholder="john.doe@example.com" value="<?= (isset($email) && $email !== '') ? $email : '' ?>">
                 </div>
+            </div>
+
+            <div class="container section">
                 <div class="<?= (isset($password) && $password === '') ? 'error-input' : ''; ?>">
                     <label for="password">Wachtwoord</label>
                     <input type="password" name="password" placeholder="*******" value="<?= (isset($password) && $password !== '') ? $password : '' ?>">
                 </div>
+                <div class="<?= (isset($password_confirm) && $password_confirm === '') ? 'error-input' : ''; ?>">
+                    <label for="password-confirm">Herhaal wachtwoord</label>
+                    <input type="password" name="password-confirm" placeholder="*******" value="<?= (isset($password_confirm) && $password_confirm !== '') ? $password_confirm : '' ?>">
+                </div>
             </div>
             <div class="container">
-                <button type="submit" name="submit">Login</button>
+                <button type="submit" name="submit">Registreer</button>
                 <div>
-                    <p>Nog geen inlog? <a href="/portal/register">Registreer nu!</a></p>
+                    <p>Al een inlog? <a href="/portal/login">Ga naar login!</a></p>
                 </div>
             </div>
         </form>
