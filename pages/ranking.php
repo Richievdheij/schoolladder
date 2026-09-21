6dade9ac0a5e8477d8d79b7ddf80629f92090879
Mariel <ruwaardmarielle@gmail.com>
2026-09-21 14:59:49 +0200

    kleine aanpassingen

--- pages/ranking.php
+++ pages/ranking.php
@@ -13,6 +13,26 @@ require_once 'includes/config.php';
 
     <main>
 
+        <section id="your-position" class="section container">
+            <h1>Ranglijst</h1>
+            <p>Bekijk de stand in jouw klas, jaarlaag en school. Zie wat je wint of verliest per positie.</p>
+
+            <div class="container">
+                <h2>Positie</h2>
+                <!--                <p>--><?php
+                //                echo $position;
+                //                ?><!--</p>-->
+            </div>
+        </section>
+
+        <div>
+            <button>Klas</button>
+            <button>Jaarlaag</button>
+            <button>School</button>
+        </div>
+
+        <section id="ranking" class="section container"></section>
+
     </main>
 
     <?php require __DIR__ . '/../includes/footer.php'; ?>
