d78bf06ea3c619863bd38c41a996c071389a08ae
Martijn Swarte <martijn.swarte@gmail.com>
2026-09-22 11:53:29 +0200

    [FIX] Users table change

--- database.sql
+++ database.sql
@@ -3,18 +3,18 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 21 sep 2026 om 11:12
--- Serverversie: 8.4.2
--- PHP-versie: 8.4.21
+-- Generation Time: Sep 22, 2026 at 09:52 AM
+-- Server version: 8.4.2
+-- PHP Version: 8.4.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -24,263 +24,249 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `classes`
+-- Table structure for table `classes`
 --
 
-CREATE TABLE `classes`
-(
-    `id`         int UNSIGNED                           NOT NULL,
-    `name`       varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-    `created_at` timestamp                              NOT NULL,
-    `updated_at` timestamp                              NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `classes` (
+  `id` int UNSIGNED NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `events`
+-- Table structure for table `events`
 --
 
-CREATE TABLE `events`
-(
-    `id`         int UNSIGNED                            NOT NULL,
-    `start_time` datetime                                NOT NULL,
-    `end_time`   datetime                                NOT NULL,
-    `name`       varchar(50) COLLATE utf8mb4_general_ci  NOT NULL,
-    `class_id`   int UNSIGNED                            NOT NULL,
-    `teacher_id` int UNSIGNED                            NOT NULL,
-    `subject_id` int UNSIGNED                            NOT NULL,
-    `created_at` timestamp                               NOT NULL,
-    `updated_at` timestamp                               NOT NULL,
-    `location`   varchar(100) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `events` (
+  `id` int UNSIGNED NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `class_id` int UNSIGNED NOT NULL,
+  `teacher_id` int UNSIGNED NOT NULL,
+  `subject_id` int UNSIGNED NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL,
+  `location` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `grades`
+-- Table structure for table `grades`
 --
 
-CREATE TABLE `grades`
-(
-    `id`         int UNSIGNED NOT NULL,
-    `student_id` int UNSIGNED NOT NULL,
-    `subject_id` int UNSIGNED NOT NULL,
-    `grade`      tinyint      NOT NULL,
-    `created_at` timestamp    NOT NULL,
-    `updated_at` timestamp    NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `grades` (
+  `id` int UNSIGNED NOT NULL,
+  `student_id` int UNSIGNED NOT NULL,
+  `subject_id` int UNSIGNED NOT NULL,
+  `grade` tinyint NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `students`
+-- Table structure for table `students`
 --
 
-CREATE TABLE `students`
-(
-    `id`         int UNSIGNED NOT NULL,
-    `user_id`    int UNSIGNED NOT NULL,
-    `points`     tinyint      NOT NULL,
-    `year_group` tinyint      NOT NULL,
-    `class_id`   int UNSIGNED NOT NULL,
-    `created_at` timestamp    NOT NULL,
-    `updated_at` timestamp    NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `students` (
+  `id` int UNSIGNED NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `points` tinyint NOT NULL,
+  `year_group` tinyint NOT NULL,
+  `class_id` int UNSIGNED NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `subjects`
+-- Table structure for table `subjects`
 --
 
-CREATE TABLE `subjects`
-(
-    `id`           int UNSIGNED                           NOT NULL,
-    `name`         varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-    `abbreviation` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
-    `created_at`   timestamp                              NOT NULL,
-    `updated_at`   timestamp                              NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `subjects` (
+  `id` int UNSIGNED NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `abbreviation` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tests`
+-- Table structure for table `tests`
 --
 
-CREATE TABLE `tests`
-(
-    `id`         int UNSIGNED NOT NULL,
-    `event_id`   int UNSIGNED NOT NULL,
-    `created_at` timestamp    NOT NULL,
-    `updated_at` timestamp    NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `tests` (
+  `id` int UNSIGNED NOT NULL,
+  `event_id` int UNSIGNED NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `users`
-(
-    `id`         int UNSIGNED                            NOT NULL,
-    `name`       varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-    `email`      varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-    `password`   varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-    `role`       varchar(20) COLLATE utf8mb4_general_ci  NOT NULL,
-    `created_at` timestamp                               NOT NULL,
-    `updated_at` timestamp                               NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `users` (
+  `id` int UNSIGNED NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `role` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'student',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexen voor geëxporteerde tabellen
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`, `updated_at`) VALUES
+(1, 'test', 'test@test', '$2y$12$QPB5l7LQVne1o.pCzTEmveio2jIicwaNLZDvoOgpHMRbLHPmvYjKC', 'student', '2026-09-22 09:18:53', NULL);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indexen voor tabel `classes`
+-- Indexes for table `classes`
 --
 ALTER TABLE `classes`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexen voor tabel `events`
+-- Indexes for table `events`
 --
 ALTER TABLE `events`
-    ADD PRIMARY KEY (`id`),
-    ADD KEY `class_id` (`class_id`),
-    ADD KEY `subject_id` (`subject_id`),
-    ADD KEY `teacher_id` (`teacher_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `class_id` (`class_id`),
+  ADD KEY `subject_id` (`subject_id`),
+  ADD KEY `teacher_id` (`teacher_id`);
 
 --
--- Indexen voor tabel `grades`
+-- Indexes for table `grades`
 --
 ALTER TABLE `grades`
-    ADD PRIMARY KEY (`id`),
-    ADD KEY `student_id` (`student_id`),
-    ADD KEY `subject_id` (`subject_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `subject_id` (`subject_id`);
 
 --
--- Indexen voor tabel `students`
+-- Indexes for table `students`
 --
 ALTER TABLE `students`
-    ADD PRIMARY KEY (`id`),
-    ADD KEY `class_id` (`class_id`),
-    ADD KEY `user_id` (`user_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `class_id` (`class_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
--- Indexen voor tabel `subjects`
+-- Indexes for table `subjects`
 --
 ALTER TABLE `subjects`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexen voor tabel `tests`
+-- Indexes for table `tests`
 --
 ALTER TABLE `tests`
-    ADD PRIMARY KEY (`id`),
-    ADD KEY `event_id` (`event_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `event_id` (`event_id`);
 
 --
--- Indexen voor tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT voor geëxporteerde tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT voor een tabel `classes`
+-- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
-    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT voor een tabel `events`
+-- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT voor een tabel `grades`
+-- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT voor een tabel `students`
+-- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT voor een tabel `subjects`
+-- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT voor een tabel `tests`
+-- AUTO_INCREMENT for table `tests`
 --
 ALTER TABLE `tests`
-    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT voor een tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Beperkingen voor geëxporteerde tabellen
+-- Constraints for dumped tables
 --
 
 --
--- Beperkingen voor tabel `events`
+-- Constraints for table `events`
 --
 ALTER TABLE `events`
-    ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-    ADD CONSTRAINT `events_ibfk_3` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-    ADD CONSTRAINT `events_ibfk_4` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `events_ibfk_3` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `events_ibfk_4` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
--- Beperkingen voor tabel `grades`
+-- Constraints for table `grades`
 --
 ALTER TABLE `grades`
-    ADD CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-    ADD CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+  ADD CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
--- Beperkingen voor tabel `students`
+-- Constraints for table `students`
 --
 ALTER TABLE `students`
-    ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-    ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
--- Beperkingen voor tabel `tests`
+-- Constraints for table `tests`
 --
 ALTER TABLE `tests`
-    ADD CONSTRAINT `tests_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+  ADD CONSTRAINT `tests_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
