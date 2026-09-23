67cb50fe91926e45496731e2fadc02c3e26bfc1a
OliviaRijkes <1101595@hr.nl>
2026-09-23 12:46:31 +0200

    schedule page: all function basis are doen and styling

--- rooster/index.php
+++ rooster/index.php
@@ -25,7 +25,7 @@ function getWeekEvents($weekOffset)
     $weekEnd = strtotime("sunday $weekOffset week 23:00");
 
     //connect db
-    require __DIR__ . '/../includes/config.php';    //gives a db as variable (not $db)
+    require_once __DIR__ . '/../includes/config.php';    //gives a db as variable (not $db)
 
     //query to get events of the week
     //need to join the teacher name and the subject name and only from the right class
@@ -34,33 +34,37 @@ function getWeekEvents($weekOffset)
     $result = mysqli_query(db, $query);
     $weekEvents = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    //close the db
-    mysqli_close(db);
-
     return $weekEvents;
 }
 
 function makeEvent(){
-//    $startTime;
-//    $endTime;
-//    $name;
-//    $classId;
-//    $teacherId;
-//    $subjectId;
-//    $location;
+    $startTime = '2026-09-23 09:17:51';
+    $endTime = '2026-09-23 12:17:51';
+    $name = 'test';
+    $classId = 1;
+    $teacherId = 1;
+    $subjectId =1;
+    $location ='hier';
 
-    require __DIR__ . '/../includes/config.php';
-//    $query = ""
+    $query = "INSERT INTO `events`(`start_time`,, `end_time`, `name`, `class_id`, `teacher_id`, `subject_id`, `location`)
+VALUES ('$startTime','$endTime','$name','$classId','$teacherId','$subjectId','$location')";
+    $result = mysqli_query(db, $query);
+
 }
 $weekEvents = getWeekEvents($weekOffset);
+//makeEvent();
+print_r(date('Y-m-d H:i:s'));
+//close the db
+mysqli_close(db);
 
 
-//get the array for the currentDaySchedule
+//get the array for the currentDaySchedule (in the text added)
 
 
 ?>
 <!DOCTYPE html>
 <html lang="nl">
+<link rel="stylesheet" href="../css/pages/rooster.css">
 
 <?php require __DIR__ . '/../includes/header.php'; ?>
 <body>
@@ -68,7 +72,12 @@ $weekEvents = getWeekEvents($weekOffset);
 <main>
     <section id="schedule-box">
         <h1>Rooster</h1>
-        <p>uitleg</p>
+        <p>Bekijk je lessen, komende toetsen en waar je extra aandacht nodig hebt.</p>
+        <div>
+            <?php for($i = 0; $i < 5; $i++){?>
+            <button>W<?=$currentWeekNumber+$i?></button>
+            <?php }?>
+        </div>
         <table>
             <tr>
                 <td>W<?= $currentWeekNumber ?></td>
@@ -83,9 +92,9 @@ $weekEvents = getWeekEvents($weekOffset);
             <?php
             //if the timestamp is in the array put in the info inside
             //else empty
-            ?>
-            <tr>
-                <th>8:00</th>
+            for($i = 9; $i <= 17; $i++){ ?>
+                <tr>
+                <th><?=$i?>:00</th>
                 <td>
                     <!--subjectAbbreviation-->
                 </td>
@@ -94,9 +103,21 @@ $weekEvents = getWeekEvents($weekOffset);
                 <td></td>
                 <td></td>
             </tr>
+            <?php } ?>
+
         </table>
     </section>
-    <section id="day-schedule list">
+    <section id="day-schedule-list">
+        <div class="day-event">
+            <h3>Naam - Vak</h3>
+            <p>leraar - locatie</p>
+            <h4>start - eind</h4>
+        </div>
+        <div class="day-event">
+            <h3>Naam - Vak</h3>
+            <p>leraar - locatie</p>
+            <h4>start - eind</h4>
+        </div>
         <!--the amount based of lessons based of today-->
         <?php
         //foreach currentDaySchedule as here-under
